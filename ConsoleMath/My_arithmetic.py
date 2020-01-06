8c4a14151ba6ac3c5fe588e1cdac63d64a3119ef
Gusarov2906 <58394539+Gusarov2906@users.noreply.github.com>
2020-01-06 21:39:26 +0300

    New stable version, negative numbers added

--- ConsoleMath/My_arithmetic.py
+++ ConsoleMath/My_arithmetic.py
@@ -1,5 +1,6 @@
 from MathStr import My_converting
 
+
 # Умножение
 def my_multiply(x, y):
     try:
@@ -142,27 +143,35 @@ def my_addition_and_subtraction_for_mas(mas):
     flag1 = False
     while True:
         for i in range(len(mas)):
-            if mas[i] == "+":
-                flag1 = True
-                mas[i] = my_addition(mas[i - 1], mas[i + 1])
-                mas[i - 1] = " "
-                mas[i + 1] = mas[i]
-                mas[i] = " "
-                # print(mas)
-                continue
-            if mas[i] == "-":
-                flag1 = True
-                mas[i] = my_subtraction(mas[i - 1], mas[i + 1])
-                mas[i - 1] = " "
-                mas[i + 1] = mas[i]
-                mas[i] = " "
-                # print(mas)
-                continue
+            if i > 0:
+                if mas[i] == "+":
+                    flag1 = True
+                    mas[i] = my_addition(mas[i - 1], mas[i + 1])
+                    mas[i - 1] = " "
+                    mas[i + 1] = mas[i]
+                    mas[i] = " "
+                    # print(mas)
+                    continue
+                if mas[i] == "-":
+                    flag1 = True
+                    mas[i] = my_subtraction(mas[i - 1], mas[i + 1])
+                    mas[i - 1] = " "
+                    mas[i + 1] = mas[i]
+                    mas[i] = " "
+                    # print(mas)
+                    continue
             flag1 = False
         mas = My_converting.convert_to_str(mas)
         mas = My_converting.convert_to_mas(mas)
         if flag1 is False:
             break
+    return mas
+
+
+def solve_without_parentheses(mas):
+    mas = my_exponentiation_for_mas(mas)
+    mas = my_division_and_multiply_for_mas(mas)
+    mas = my_addition_and_subtraction_for_mas(mas)
     return mas
 
 
--- ConsoleMath/My_converting.py
+++ ConsoleMath/My_converting.py
@@ -25,7 +25,7 @@ def is_arithmetic_sign(char):
 
 # bool функция на проверку на арифметический символ
 def is_parentheses_sign(char):
-    if char == "a" or char == "e":
+    if char == "a" or char == "e" or char == f'a{int}' or char == "en":
         return True
     return False
 
@@ -41,34 +41,56 @@ def is_indentation(char):
 def convert_to_mas(str_value):
     mas = []
     val = ""
-    flag = False
-    flag2 = False
+    is_negative = False
+    is_number = False
+    is_parentheses = False
     for i in range(len(str_value)):
-        if flag2:
-            flag2 = False
+        if is_parentheses:
+            is_parentheses = False
             continue
+        if i == 0:
+            if str_value[i] == "-":
+                is_negative = True
+        else:
+            if is_arithmetic_sign(str_value[i - 1]) or is_indentation(str_value[i-1]):
+                if str_value[i] == "-":
+                    is_negative = True
         if str_value[i].isdigit() or str_value[i] == '.':
             val += str_value[i]
-            flag = True
+            is_number = True
             continue
-        if flag:
+        if str_value[i] == '-':
+            if is_negative:
+                val += str_value[i]
+                is_number = True
+                is_negative = False
+                continue
+        if is_number:
             mas.append(val)
-            flag = False
+            is_number = False
         if is_arithmetic_sign(str_value[i]):
             mas.append(str_value[i])
         elif i < len(str_value) - 1:
             if is_parentheses_sign(str_value[i]):
                 mas.append(str_value[i] + str_value[i + 1])
-                flag2 =True
+                is_parentheses = True
         elif is_indentation(str_value[i]):
             continue
         else:
             print("Invalid symbol")
             return None
         val = ""
-    if flag:
+    if is_number:
         mas.append(val)
     return mas
+
+
+# функция проверяет на наличие скобок
+def has_parentheses(str_value):
+    for i in range(len(str_value)):
+        if is_parentheses_sign(str_value[i]):
+            return True
+    return False
 
 
 # функция собирает из массива строку
@@ -77,4 +99,3 @@ def convert_to_str(mas):
     for i in range(len(mas)):
         string += str(mas[i])
     return string
-
