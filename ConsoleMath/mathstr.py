98601daba83f87da5040a57d066d0d7422d7ce54
Gusarov2906 <58394539+Gusarov2906@users.noreply.github.com>
2020-01-07 13:06:09 +0300

    New interface for user Add new commands

--- ConsoleMath/mathstr.py
+++ ConsoleMath/mathstr.py
@@ -2,25 +2,35 @@ from MathStr import My_math
 from MathStr import My_converting
 from MathStr import Database
 
+
 def main():
-    print("ConsoleMATH by Andrey Gusarov\nIf you want to finish, write 'exit'")
+    print(
+        "ConsoleMATH by Andrey Gusarov\nIf you want to see all commands, write 'help'\n"
+        "If you want to finish, write 'exit'")
     database = Database.Database()
     database.load_from_file()
-    database.print()
     while True:
         string = input(">>> ")
         if string == 'exit':
             break
+        elif string == 'help':
+            print(
+                "Commands:\nhelp - All commands\nhistory - Show 10 recent expressions\n"
+                "clear - Clear all history\nexit - Finish work")
+            continue
+        elif string == 'history':
+            database.print()
+            continue
+        elif string == "clear":
+            database.clear()
+            continue
         database.add_expression(string)
         string = My_math.solve(string)
         if string is not None:
             string = My_converting.convert_to_str(string)
             print(f'Result: {string} ')
         database.add_result(string)
-
-    database.print()
     database.save_to_file()
-
 
 
 """    mas = convert_to_mas(string)
